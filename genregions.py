3573a8b3fe09391de5df1318c5211c5b85a7f938
Anshu Prateek <git@anshuprateek.com>
2024-08-24 21:28:30 +0530

    Claude generated script
    Fixes bug of unsolvable regions

--- genregions.py
+++ genregions.py
@@ -1,99 +1,168 @@
-import numpy as np
-from collections import deque
 import random
+import hashlib
+from collections import deque
 
-# In-memory cache for checksums
 checksum_cache = set()
+DEBUG = bool(False)
+max_region_size = 10
 
-def generate_random_queens_positions(size):
-    """Generate a random valid solution for the 8-queens problem."""
-    def is_valid(queens, row, col):
-        for r, c in queens:
-            if c == col or abs(r - row) == abs(c - col):
-                return False
-        return True
+def debug_print(*args):
+    if DEBUG:
+        print(args)
 
-    def place_queens(row):
-        if row == size:
-            return True
-        cols = list(range(size))
-        random.shuffle(cols)  # Randomize the order of columns
-        for col in cols:
-            if is_valid(queens, row, col):
-                queens.append((row, col))
-                board[row][col] = 1
-                if place_queens(row + 1):
-                    return True
-                queens.pop()
-                board[row][col] = 0
-        return False
+def is_safe(board, row, col):
+    for i in range(8):
+        if board[row][i] == 1 or board[i][col] == 1:
+            return False
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    for i, j in zip(range(row, -1, -1), range(col, 8)):
+        if board[i][j] == 1:
+            return False
+    return True
 
-    board = np.zeros((size, size), dtype=int)
+def solve_queens():
+    board = [[0 for _ in range(8)] for _ in range(8)]
     queens = []
-    place_queens(0)
-    return board
+    
+    def backtrack(row):
+        if row == 8:
+            return True
+        cols = list(range(8))
+        random.shuffle(cols)
+        for col in cols:
+            if is_safe(board, row, col):
+                board[row][col] = 1
+                queens.append((row, col))
+                if backtrack(row + 1):
+                    return True
+                board[row][col] = 0
+                queens.pop()
+        return False
+    
+    backtrack(0)
+    return queens, board
 
-def generate_regions(board):
-    size = len(board)
-    region_id = 1
-    regions = np.zeros((size, size), dtype=int)
-    visited = np.zeros((size, size), dtype=bool)
+def get_neighbors(r, c):
+    return [(r+1, c), (r-1, c), (r, c+1), (r, c-1)]
 
-    def bfs(start_row, start_col):
-        nonlocal region_id
-        queue = deque([(start_row, start_col)])
-        regions[start_row][start_col] = region_id
-        cells = [(start_row, start_col)]
-        visited[start_row][start_col] = True
+def create_region_map(queens):
+    def get_neighbors(r, c):
+        return [(r+1, c), (r-1, c), (r, c+1), (r, c-1)]
+
+    def is_valid_and_empty(r, c):
+        return 0 <= r < 8 and 0 <= c < 8 and region_map[r][c] == 0
+
+    def bfs_fill(start_r, start_c, color):
+        queue = deque([(start_r, start_c)])
+        region_map[start_r][start_c] = color
+        cells_filled = 1
+
+        while queue and cells_filled < max_region_size:
+            r, c = queue.popleft()
+            for nr, nc in get_neighbors(r, c):
+                if is_valid_and_empty(nr, nc):
+                    region_map[nr][nc] = color
+                    queue.append((nr, nc))
+                    cells_filled += 1
+                    if cells_filled == max_region_size:
+                        break
+
+    region_map = [[0 for _ in range(8)] for _ in range(8)]
+    colors = list(range(1, 9))
+    random.shuffle(colors)
+
+    # Place queens and start regions
+    for (r, c), color in zip(queens, colors):
+        bfs_fill(r, c, color)
+
+    # Fill remaining cells
+    for color in colors:
+        while True:
+            empty_cells = [(r, c) for r in range(8) for c in range(8) if region_map[r][c] == 0]
+            if not empty_cells:
+                break
+            start_r, start_c = random.choice(empty_cells)
+            bfs_fill(start_r, start_c, color)
+
+        return region_map
+
+def calculate_checksum(queens, region_map):
+    board_str = ''.join([''.join(map(str, row)) for row in region_map])
+    queens_str = ''.join([f"{r}{c}" for r, c in queens])
+    combined_str = board_str + queens_str
+    return hashlib.md5(combined_str.encode()).hexdigest()
+
+def generate_solution():
+    queens, queens_board = solve_queens()
+    region_map = create_region_map(queens)
+    checksum = calculate_checksum(queens, region_map)
+    return queens, queens_board, region_map, checksum
+
+def print_board(board):
+    for row in board:
+        print(row, ",")
+    print()
+    
+
+def count_regions(region_map):
+    regions = set()
+    for row in region_map:
+        regions.update(row)
+    return len(regions)
+
+def color_distribution(region_map):
+    color_counts = {}
+    for row in region_map:
+        for color in row:
+            color_counts[color] = color_counts.get(color, 0) + 1
+    return color_counts
+
+def check_contiguous_regions(region_map):
+    def get_neighbors(r, c):
+        return [(r+1, c), (r-1, c), (r, c+1), (r, c-1)]
+
+    def bfs(start_r, start_c, color, visited):
+        queue = deque([(start_r, start_c)])
+        visited.add((start_r, start_c))
+        region_size = 1
+
         while queue:
             r, c = queue.popleft()
-            for nr, nc in [(r-1, c), (r+1, c), (r, c-1), (r, c+1)]:
-                if 0 <= nr < size and 0 <= nc < size and board[nr][nc] == 1 and not visited[nr][nc]:
-                    regions[nr][nc] = region_id
-                    visited[nr][nc] = True
+            for nr, nc in get_neighbors(r, c):
+                if 0 <= nr < 8 and 0 <= nc < 8 and region_map[nr][nc] == color and (nr, nc) not in visited:
                     queue.append((nr, nc))
-                    cells.append((nr, nc))
-        return cells
+                    visited.add((nr, nc))
+                    region_size += 1
 
-    # First pass to assign regions
-    for row in range(size):
-        for col in range(size):
-            if board[row][col] == 1 and regions[row][col] == 0:
-                cells_in_region = bfs(row, col)
-                if len(cells_in_region) == 1:
-                    # Reassign to a new region ID if necessary
-                    region_id += 1
-                    regions[cells_in_region[0][0]][cells_in_region[0][1]] = region_id
-                region_id += 1
+        return region_size
 
-    # Ensure no zero regions and most regions have more than one cell
-    new_region_id = region_id + 1
+    visited = set()
+    color_regions = {}
 
-    for row in range(size):
-        for col in range(size):
-            if regions[row][col] == 0:
-                # Assign a region number based on neighboring cells
-                neighboring_ids = set()
-                for nr, nc in [(row-1, col), (row+1, col), (row, col-1), (row, col+1)]:
-                    if 0 <= nr < size and 0 <= nc < size and regions[nr][nc] != 0:
-                        neighboring_ids.add(regions[nr][nc])
-
-                if neighboring_ids:
-                    regions[row][col] = random.choice(list(neighboring_ids))
+    for r in range(8):
+        for c in range(8):
+            if (r, c) not in visited:
+                color = region_map[r][c]
+                region_size = bfs(r, c, color, visited)
+                
+                if color not in color_regions:
+                    color_regions[color] = [region_size]
                 else:
-                    regions[row][col] = new_region_id
-                    new_region_id += 1
+                    color_regions[color].append(region_size)
 
-    return regions
+    non_contiguous = {color: sizes for color, sizes in color_regions.items() if len(sizes) > 1}
 
-def calculate_checksum(array):
-    """Calculate a checksum for a 2D numpy array."""
-    flat_array = array.flatten()
-    return hash(tuple(flat_array))
+    if non_contiguous:
+        debug_print("Non-contiguous regions found:")
+        for color, sizes in non_contiguous.items():
+            debug_print(f"Color {color}: {len(sizes)} disconnected regions of sizes {sizes}")
+        return False
+    else:
+        debug_print("All regions are contiguous.")
+        return True
 
-def format_array(array):
-    """Format the 2D numpy array for consistent printing and saving."""
-    return "[\n" + "\n".join(f"    {list(row)}," for row in array.tolist()) + "\n]"
 
 def save_to_file(filename, board, checksum, regions_formatted):
     """Save the board and its checksum to a file."""
@@ -106,50 +175,52 @@ def save_to_file(filename, board, checksum, regions_formatted):
         if len(regions_formatted)>1:
             # file.write("\nFormatted Regions:\n")
             # regions_formatted=regions_formatted+","
-            region = "("+str(checksum) +","+str(regions_formatted)+"),"
+            region = "\""+str(checksum) +"\":"+str(regions_formatted)+","
             file.write(region)
         # file.write(f"\nChecksum: {checksum}\n")
 
-
-def main():
-    size = 8
-    board = generate_random_queens_positions(size)
-    region_colors = generate_regions(board)
-
-    # Calculate checksums
-    board_checksum = calculate_checksum(board)
-    regions_checksum = calculate_checksum(region_colors)
-
-    # Check if the checksums are already in the cache
-    if board_checksum in checksum_cache or regions_checksum in checksum_cache:
-        print("Duplicate checksum detected. Skipping save.")
-        return
-
-    # Add new checksums to the cache
-    checksum_cache.add(board_checksum)
-    checksum_cache.add(regions_checksum)
-
-    # Format the regions output
-    regions_formatted = format_array(region_colors)
-
-    # Save to files
-    save_to_file('queens.txt', board, str(board_checksum)+"|"+str(regions_checksum), '')
-    save_to_file('regions.txt', '', regions_checksum, regions_formatted)
-
-    # Print to console as well
-    print("Queens Board:")
-    for row in board:
-        print(' '.join(map(str, row)))
-    
-    print("\nRegion Colors:")
-    print(regions_formatted)
-
-    print(f"\nChecksum of Queens Board: {board_checksum}")
-    print(f"Checksum of Region Colors: {regions_checksum}")
-
-# Generate, print, and save the board with regions and checksums
-
+# Generate and debug_print the solution
+# queens, queens_board, region_map, checksum = generate_solution()
+# is_region_contiguous = check_contiguous_regions(region_map)
+# debug_print(check_contiguous_regions(region_map))
+is_region_contiguous = False
 c=0
-while(c<1000):
-    c=c+1
-    main()
+num_boards=100
+while not is_region_contiguous or c < num_boards:
+    debug_print(c)
+    queens, queens_board, region_map, checksum = generate_solution()
+    if checksum in checksum_cache:
+        debug_print("Duplicate checksum detected. Skipping checks.")
+        continue
+    checksum_cache.add(checksum)
+    is_region_contiguous = check_contiguous_regions(region_map)
+    if is_region_contiguous:
+        c=c+1
+        print_board(region_map)
+        save_to_file('queens.txt', queens_board, checksum, '')
+        save_to_file('regions.txt', '', checksum, region_map)
+
+        
+        # print(c)
+        
+
+
+
+
+# debug_print("Queens positions (list):")
+# debug_print(queens)
+# debug_print("\nQueens positions (8x8 matrix):")
+# debug_print_board(queens_board)
+# debug_print("Region map:")
+# debug_print_board(region_map)
+
+# if not is_region_contiguous:
+#     debug_print("The region map contains non-contiguous regions and needs to be regenerated.")
+# else:
+#     debug_print("The region map is valid with all contiguous regions.")
+
+# debug_print(f"Number of regions: {count_regions(region_map)}")
+# debug_print("Color distribution:")
+# for color, count in color_distribution(region_map).items():
+#     debug_print(f"Color {color}: {count} cells")
+# debug_print(f"Checksum: {checksum}")
